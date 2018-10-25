f46eecd51d610e8f6519746b181850fcbfab547d
nickyc975 <chenjinlong2016@gmail.com>
2018-10-25 20:01:01 +0800

    Lab-2: gradient with regular.

--- Lab_2_logistic_regression/main.py
+++ Lab_2_logistic_regression/main.py
@@ -1,22 +1,26 @@
 import numpy
 
 DATA_SCALE = 50
-DIMENSION = 10
+DIMENSION = 5
 
-ETA = 0.01
-DELTA = 1e-7
+SCALE = 0.3
+POSITIVE = 0.8
+NEGATIVE = 0.3
+
+ETA = 1e-2
+LAMBDA = 1e-3
+DELTA = 1e-6
 
 
 def get_data(data_scale):
-    X = []
-    Y = []
+    X, Y = [], []
     for i in range(0, data_scale):
         x = [1]
-        x.extend(numpy.random.normal(0.8, 0.1, 10))
+        x.extend(numpy.random.normal(POSITIVE, SCALE, DIMENSION))
         X.append(x)
         Y.append(1)
         x = [1]
-        x.extend(numpy.random.normal(0.3, 0.1, 10))
+        x.extend(numpy.random.normal(NEGATIVE, SCALE, DIMENSION))
         X.append(x)
         Y.append(0)
     return X, Y
@@ -24,24 +28,35 @@ def get_data(data_scale):
 
 def gradient_ascent(X_mat, Y_mat):
     delta = 1
-    W = numpy.mat([0] * (DIMENSION + 1)).T
+    W_mat = numpy.mat([0] * (DIMENSION + 1)).T
     while delta > DELTA:
-        V = []
-        for i in range(0, 2 * DATA_SCALE):
-            temp = numpy.exp(W.T * X_mat[:, i])[0, 0]
-            V.append(temp/(1 + temp))
-        V_mat = Y_mat - numpy.mat(V).T
-        delta_mat = ETA * X_mat * V_mat
-        W = W + delta_mat
+        exp_xTw = numpy.exp(X_mat.T * W_mat)
+        delta_mat = ETA * X_mat * (Y_mat - exp_xTw / (1 + exp_xTw))
         delta = delta_mat.T * delta_mat
-    return W
+        W_mat = W_mat + delta_mat
+    return W_mat
+
+
+def gradient_ascent_regular(X_mat, Y_mat):
+    delta = 1
+    W_mat = numpy.mat([0] * (DIMENSION + 1)).T
+    while delta > DELTA:
+        exp_xTw = numpy.exp(X_mat.T * W_mat)
+        delta_mat = ETA * X_mat * (Y_mat - exp_xTw / (1 + exp_xTw))
+        W_mat = W_mat - ETA * LAMBDA * W_mat + delta_mat
+        delta = delta_mat.T * delta_mat
+    return W_mat
+
+
+def newton_method(X_mat, Y_mat):
+    pass
 
 
 X, Y = get_data(DATA_SCALE)
 
 X_mat, Y_mat = numpy.mat(X).T, numpy.mat(Y).T
 
-W_mat = gradient_ascent(X_mat, Y_mat)
+W_mat = gradient_ascent_regular(X_mat, Y_mat)
 
 test_X, test_Y = get_data(5)
 
