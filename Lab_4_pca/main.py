a8324c6f6f1fc2293b040b8aeee0775e7daa5fbe
nickyc975 <chenjinlong2016@gmail.com>
2018-11-15 11:07:50 +0800

    Lab-4: Added 3D scatter.

--- Lab_4_pca/main.py
+++ Lab_4_pca/main.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 DATA_SCALE = 500
 MEANS = [1, 2, 3]
@@ -17,8 +18,17 @@ def pca(x, k):
 
 data = generate_data(MEANS, VARIANCES, DATA_SCALE)
 eigenvectors = pca(data, len(MEANS) - 1)
-projected_data = numpy.transpose(numpy.dot(data, eigenvectors))
 
-fig, axs = plt.subplots(1, 1)
-axs.scatter(projected_data[0], projected_data[1])
+data = data.T
+projected_data = numpy.dot(eigenvectors.T, data)
+
+axs1 = plt.subplot(111, projection='3d')
+axs1.set_xlim(MEANS[0] - 1, MEANS[0] + 1)
+axs1.set_ylim(MEANS[1] - 1, MEANS[1] + 1)
+axs1.set_zlim(MEANS[2] - 1, MEANS[2] + 1)
+axs1.scatter(data[0], data[1], data[2])
+
+fig, axs2 = plt.subplots(1, 1)
+axs2.scatter(projected_data[0], projected_data[1])
+
 plt.show()
